ac3b19b2e0a5d3de6efcb0d72524b8147e2f87bb
SakSecret <105237495+SakSecret@users.noreply.github.com>
2022-05-09 13:59:31 -0400

    Add files via upload

---
+++ ProjectMain.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class ProjectMain extends JFrame {
+
+	public ProjectMain() {
+		setBounds(100, 100, 1000, 1000);
+		
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new ProjectMain();
+	}
+
+}
