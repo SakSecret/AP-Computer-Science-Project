3d00895c9053150b590e01b1a5353c8b4812764a
SakSecret <105237495+SakSecret@users.noreply.github.com>
2022-05-11 13:50:33 -0400

    Update ProjectMain.java

--- ProjectMain.java
+++ ProjectMain.java
@@ -1,11 +1,23 @@
 import java.awt.*;
-import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 
-public class ProjectMain extends JFrame {
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.*;
+public class ProjectMain extends JFrame{
 
 	public ProjectMain() {
-		setBounds(100, 100, 1000, 1000);
+		setBounds(100, 100, 1000, 500);
+		setLayout(null);
 		
+		
+		
+		
+		Player player = new Player(200, 200);
+		add(player);
+		
+
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
@@ -13,6 +25,10 @@ public class ProjectMain extends JFrame {
 	
 	public static void main(String[] args) {
 		new ProjectMain();
+	}
+	
+	public void actionPerformed() {
+		
 	}
 
 }
