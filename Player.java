492a70357496a3431d2b8a13bd9957be641e0581
SakSecret <105237495+SakSecret@users.noreply.github.com>
2022-05-12 14:09:28 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -6,12 +6,15 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 public class Player extends JPanel {
+	private Rectangle hitbox;
 	private JLabel picLabel;
 	private BufferedImage myPicture;
 	private double xVelocity = 0;
 	private double yVelocity = 0;
 	private boolean airborne = false;
 	private Insets insets;
+	private final int MAX_VELOCITY = 10;
+	private final int TERMINAL_VELOCITY = 10;
 	public Player(int x, int y) {
 		setLocation(x, y);
 		setSize(100, 100);
@@ -28,13 +31,14 @@ public class Player extends JPanel {
 		picLabel.setPreferredSize(new Dimension(100, 100));
 		add(picLabel);
 		insets = this.getInsets();
+		hitbox = new Rectangle(0, 0, 100, 100);
 		
 	}
 	public void jump() {
 		if (airborne) {
 			return;
 		}
-		yVelocity = -20;
+		yVelocity = 20;
 	}
 	public void update() {
 		if (getY() + getHeight() > getParent().getHeight() - insets.bottom - yVelocity) {
@@ -49,6 +53,31 @@ public class Player extends JPanel {
 		else {
 			yVelocity = 0;
 		}
+		setLocation(getX() + (int)xVelocity, getY() + (int)(yVelocity));
+	}
+	
+	public void accelerate(double x, double y) {
+		if (!(xVelocity > MAX_VELOCITY)) {
+			xVelocity += x;
+		}
+		if (!(yVelocity > TERMINAL_VELOCITY)) {
+			yVelocity += y;
+		}
+	}
+	
+	public void decelerate(double x, double y) {
+		if (!(xVelocity < x)) {
+			xVelocity -= x;
+		}
+		else {
+			xVelocity = 0;
+		}
+		if (!(yVelocity < y)) {
+			yVelocity -= x;
+		}
+		else {
+			yVelocity = 0;
+		}
 	}
 	public double getXVelocity() {
 		return xVelocity;
@@ -57,10 +86,10 @@ public class Player extends JPanel {
 	public double getYVelocity() {
 		return yVelocity;
 	}
-	public void setXVelocity(int val) {
+	public void setXVelocity(double val) {
 		xVelocity = val;
 	}
-	public void setYVelocity(int val) {
+	public void setYVelocity(double val) {
 		yVelocity = val;
 	}
 	
